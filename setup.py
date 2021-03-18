49ea9c1fe1d34b62261a02045bcf168265ddbb35
Mohammed Zeeshan Mulla <zeeshanmullamail@gmail.com>
2021-03-19 00:48:16 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    descriptio="its a wine Q package",
+    author= "z",
+    packages= find_packages(),
+    license="MIT"
+)
