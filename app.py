56b75d05797a40eba45367c958b7d72efaf83a57
Mohammed Zeeshan Mulla <zeeshanmullamail@gmail.com>
2021-03-20 15:00:33 +0530

    update prediction service

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from prediction_service import prediction
 import os
 import yaml
 import joblib
@@ -17,42 +18,26 @@ def read_params(config_path):
         config = yaml.safe_load(yaml_file)
     return config
 
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config["webapp_model_dir"]
-    model = joblib.load(model_dir_path)
-    prediction = model.predict(data)
-    print(prediction)
-    return prediction[0]
-
-def api_response(request):
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = {"response":response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error": "Something went wrong!! Try again"}
-        return error
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         try:
             if request.form:
+                data_req = dict(request.form)
                 data = dict(request.form).values()
                 data = [list(map(float, data))]
                 response = predict(data)
                 return render_template("index.html", response=response)
 
             elif request.json:
-                response = api_response(request)
+                response = prediction.api_response(request.json)
                 return jsonify(response)
 
         except Exception as e:
             print(e)
-            error = {"error": "Something went wrong!! Try again"}
+            #error = {"error": "Something went wrong!! Try again"}
+            error = {"error": e}
             return render_template("404.html", error=error)
     else:
         return render_template("index.html")
